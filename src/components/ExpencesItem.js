3a6a02c0b355e18cd87b79cda4cd1c4650a92f27
Hassan Shaikh <hassanshaikh4461@gmail.com>
2025-03-03 12:44:39 +0530

    02march

--- src/components/ExpencesItem.js
+++ src/components/ExpencesItem.js
@@ -1,11 +1,14 @@
 import './ExpencesItem.css'
 
 function ExpencesItem(){
-return(
-    <div className='expence-item'>
+    let title = "car Insurances";
+   
+    return(
+     <div className='expence-item'>
         <div>11 Feb 2024</div>
         <div className='expence-item-descripiton'>
-            <h2>Car Insurance</h2>
+            {/* <h2>Car Insurance</h2> */}
+            <h2>{title}</h2>
             <div className='expence-item-price'>$500</div>
         </div>
     </div>
